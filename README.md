f06f25cffc05ab3a050432cc859c966cd91485f1
tiyn <tiyn@mail-mk.eu>
2023-10-24 18:48:10 +0200

    added background-image patch to config

--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 
 This is my patched version of slock. The base version is directly from suckless.org.
 
+## Patches
+
+The list below shows the currently applied patches to the master branch.
+
+- slock-background-image-20220318-1c5a538.diff (shows a background image in locked state)
+
 ## Installation
 
 The most basic way is to clone the repository and then invoke make.
 
-- `git clone https://github.com/tiyn/st-0.9.0`
+- `git clone https://github.com/tiyn/slock`
 - `make clean install`
--- config.def.h
+++ config.def.h
@@ -1,5 +1,5 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
+static const char *user  = "user";
 static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
@@ -10,3 +10,6 @@ static const char *colorname[NUMCOLS] = {
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* Background image path, should be available to the user above */
+static const char* background_image = "/home/user/.config/lock.png";
